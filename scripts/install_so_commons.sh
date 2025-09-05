39432b1d02f9d5edd7cf816cdf6e8a284377eeae
MPC139 <mpormi@frba.utn.edu.ar>
2025-09-05 16:24:18 -0300

    Remove Makefile and hello_world.c; update installation script for so-commons

--- scripts/install_so_commons.sh
+++ scripts/install_so_commons.sh
@@ -4,3 +4,5 @@
 git clone https://github.com/sisoputnfrba/so-commons-library.git /tmp/so-commons-library
 cd /tmp/so-commons-library
 sudo make install
+sudo apt-get update
+sudo apt-get install libssl-dev
